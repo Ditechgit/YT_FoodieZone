98f31502ade7724431737e0575ddadb219e8f176
Diogo Santos <diogo.jesus.tech@outlook.com>
2025-03-11 23:54:30 -0300

    feat - Component Services (Incomplete)

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar";
+import Services from "./components/Services/Services";
 
 function App() {
 
 
   return (
     <>
+      
+      
       <Navbar />
       <Hero />
+      <Services />
     </>
   );
 }
---
+++ src/components/Services/Services.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Services = () => {
+  return (
+    <>
+      <div className="py-10">
+        <div className="px-12">
+          <div className="text-center mb-20 max-w-[400px]">
+            <p>Our Services</p>
+            <h1>Services</h1>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Services;
