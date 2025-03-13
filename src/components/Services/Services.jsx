5ffe0eac2fe027bf96a6c096126764bea7e21ed7
Diogo Santos <diogo.jesus.tech@outlook.com>
2025-03-13 15:48:03 -0300

    feat - componente Services  almost complete

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -43,14 +43,18 @@ const Services = () => {
           </div>
           {/* Card Section */}
           <div>
-            <div>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-14 md:gap-5 place-item-center">
               {ServicesData.map(({ id, img, name, description }) => {
-                return <div key={id}>
-                  <div>
-                    <img src={img} alt="" />
+                return <div key={id} className="max-w-[300px] group rounded-2xl bg-white dark:bg-gray-800 hover:bg-amber-400 hover:text-white duration-300 p-4 shadow-xl">
+                  <div className="h-[100px]">
+                    <img src={img} alt="" className="max-w-[200px] mx-auto block transform -translate-y-14 group-hover:scale-105 group-hover:rotate-6 duration-300"/>
+                  </div>
+                  <div className="p-4 text-center">
+                    <h1 className="text-xl font-bold">{name}</h1>
+                    <p className="text-gray-500 group-hover:text-white duration-300 text-sm line-clamp-2">{description}</p>
                   </div>
                 </div>
-              })};
+              })}
             </div>
           </div>
         </div>
