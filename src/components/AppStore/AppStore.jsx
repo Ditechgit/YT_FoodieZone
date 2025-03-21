bca63925907040a6d4b82a8c2fb20badc25a3cd1
Diogo Santos <diogo.jesus.tech@outlook.com>
2025-03-21 15:46:06 -0300

    feat - AppStore component complete

--- src/components/AppStore/AppStore.jsx
+++ src/components/AppStore/AppStore.jsx
@@ -8,14 +8,30 @@ const AppStore = () => {
     <>
       <div className="bg-gray-100 dark:bg-gray-800 py-14">
         <div className="container">
-            <div className="grid grid-cols-1 md:grid-cols-2 items-center gap-4">
-                <div>
-                    <h1 className="text-2xl text-center sm:text-left sm:text-4xl font-semibold dark:text-gray-400 text-gray-700">Foodly is Avaible for Android and IOS</h1>
-                </div>
-                <div>
-
-                </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 items-center gap-4">
+            <div className="space-y-6 max-w-xl mx-auto">
+              <h1 className="text-2xl text-center sm:text-left sm:text-4xl font-semibold dark:text-gray-400 text-gray-700">
+                Foodly is Avaible for Android and IOS
+              </h1>
+              <div className="flex flex-wrap justify-center sm:justify-start items-center">
+                <a href="#">
+                  <img
+                    src={PlayStoreImg}
+                    alt="playstore"
+                    className="max-w-[150px] sm:max-w-[120px] md:max-w-[200px]"
+                  />
+                  <img
+                    src={AppStoreImg}
+                    alt="appstore"
+                    className="max-w-[150px] sm:max-w-[120px] md:max-w-[200px]"
+                  />
+                </a>
+              </div>
             </div>
+            <div>
+              <img src={Gif} alt="gif" className="max-w-[300px] mx-auto" />
+            </div>
+          </div>
         </div>
       </div>
     </>
