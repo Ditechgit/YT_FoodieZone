c2570177586ad0743765995d0bcd2b5f467acd78
Diogo Santos <diogo.jesus.tech@outlook.com>
2025-03-24 16:13:34 -0300

    feat - Add Testimonial component

---
+++ src/components/Testimonial/Testimonial.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Testimonial = () => {
+  return (
+    <>
+      <div className="py-10">
+        <div className="container">
+          {/* Header Section */}
+          <div className="text-center mb-20 max-w-[400px] mx-auto">
+            <p className="text-sm bg-clip-text text-transparent bg-gradient-to-r from-amber-400 to-amber-600">
+              Testimonial
+            </p>
+            <h1 className="text-3xl font-bold">Testimonial</h1>
+            <p className="text-xs text-gray-400">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo vel
+              beatae cumque autem impedit maxime, et optio atque non at!
+            </p>
+          </div>
+          {/* testimonial section */}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Testimonial;
