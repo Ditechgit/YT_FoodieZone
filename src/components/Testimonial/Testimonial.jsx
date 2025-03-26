ea7db7aee7aa3832d21049a10bb29e9f78146aeb
Diogo Santos <diogo.jesus.tech@outlook.com>
2025-03-26 13:11:42 -0300

    feat - Testimonial Complete

--- src/components/Testimonial/Testimonial.jsx
+++ src/components/Testimonial/Testimonial.jsx
@@ -1,4 +1,26 @@
 import React from "react";
+import Slider from "react-slick";
+
+const testimonialData = [
+  {
+    id: 1,
+    name: "John Doe",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt esse quod amet corrupti aperiam! Dolorum, ipsam molestiae? Incidunt, modi praesentium.",
+    img: "https://picsum.photos/101/101",
+  },
+  {
+    id: 1,
+    name: "Jane Doe",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt esse quod amet corrupti aperiam! Dolorum, ipsam molestiae? Incidunt, modi praesentium.",
+    img: "https://picsum.photos/102/102",
+  },
+  {
+    id: 1,
+    name: "John Doe",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt esse quod amet corrupti aperiam! Dolorum, ipsam molestiae? Incidunt, modi praesentium.",
+    img: "https://picsum.photos/103/103",
+  },
+];
 
 const Testimonial = () => {
   const settings = {
@@ -32,7 +54,24 @@ const Testimonial = () => {
           </div>
           {/* testimonial section */}
           <div className="grid grid-cols-1 max-w-[600px] mx-auto gap-6">
-            <Slider {...settings}></Slider>
+            <Slider {...settings}>
+              {testimonialData.map(({ id, name, text, img }) => {
+                return (
+                  <div key={id} className="my-6">
+                    <div className="flex flex-col justify-center items-center gap-4 text-center shadow-lg mx-4 rounded-xl dark:bg-gray-800 bg-yellow-100 relative">
+                      <img
+                        src={img}
+                        alt=""
+                        className="rounded-full block mx-auto"
+                      />
+                      <p className="text-gray-500 text-sm">{text}</p>
+                      <h1 className="text-xl font-bold">{name}</h1>
+                      <p className="text-gray-400 text-8xl font-serif absolute top-0 right-0">,,</p>
+                    </div>
+                  </div>
+                );
+              })}
+            </Slider>
           </div>
         </div>
       </div>
