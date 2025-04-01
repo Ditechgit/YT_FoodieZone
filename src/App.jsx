16707080f9faf0022ca2580d4354e258047817a7
Diogo Santos <diogo.jesus.tech@outlook.com>
2025-04-01 16:16:57 -0300

    feat - component Footer complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import AppStore from "./components/AppStore/AppStore";
 import Banner from "./components/Banner/Banner";
 import Footer from "./components/Footer/Footer";
@@ -5,9 +6,20 @@ import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar";
 import Services from "./components/Services/Services";
 import Testimonial from "./components/Testimonial/Testimonial";
+import "aos/dist/aos.css";
+import AOS from "aos";
 
 function App() {
 
+  useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 500,
+      easing: "ease-in-sine",
+      delay: 100,
+    });
+    AOS.refresh();
+  },[]);
 
   return (
     <>
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import FooterLogo from "../../assets/food-logo.png";
-import { FaLocationArrow, FaMobileAlt } from "react-icons/fa";
+import {
+  FaFacebook,
+  FaInstagram,
+  FaLinkedin,
+  FaLocationArrow,
+  FaMobileAlt,
+} from "react-icons/fa";
 
 const Footer = () => {
   return (
@@ -27,10 +33,56 @@ const Footer = () => {
                 <FaMobileAlt />
                 <p>+91 123456789</p>
               </div>
+              {/* Social handles */}
+              <div>
+                <div className="flex items-center gap-3 mt-6">
+                  <a href="#">
+                    <FaInstagram className="text-3xl" />
+                  </a>
+                  <a href="#">
+                    <FaFacebook className="text-3xl" />
+                  </a>
+                  <a href="#">
+                    <FaLinkedin className="text-3xl" />
+                  </a>
+                </div>
+              </div>
             </div>
-            {/* Social handles */}
-            
-            <div className="grid grid-cols-2 sm:grid-cols-3 col-span-2 md:pl-10"></div>
+            <div className="grid grid-cols-2 sm:grid-cols-3 col-span-2 md:pl-10">
+              <div>
+                <div className="py-8 px-4">
+                  <h1 className="text-xl font-bold text-justify sm:text-left mb-3" >Important Links</h1>
+                  <ul className="flex flex-col gap-3">
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Services</li>
+                    <li>Login</li>
+                  </ul>
+                </div>
+              </div>
+              <div>
+                <div className="py-8 px-4">
+                  <h1 className="text-xl font-bold text-justify sm:text-left mb-3" >Important Links</h1>
+                  <ul className="flex flex-col gap-3">
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Services</li>
+                    <li>Login</li>
+                  </ul>
+                </div>
+              </div>
+              <div>
+                <div className="py-8 px-4">
+                  <h1 className="text-xl font-bold text-justify sm:text-left mb-3" >Important Links</h1>
+                  <ul className="flex flex-col gap-3">
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Services</li>
+                    <li>Login</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
